e3bf288d19e581d84b6260dc50baecee6f00fa8f
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-06-05 23:53:04 +0000

    05/06/2024

---
+++ script.js
@@ -0,0 +1,52 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaperguntas = document.querySelector('.caixa-perguntas');
+const caxaalternativas = document.querySelector('.caixa-alternativas');
+const caixaresultado = document.querySelector('.caixa-resultado');
+const caixaresultado = document.querySelector('.caixa-resultado');
+
+const perguntas =[
+  {
+    enunciado " ":
+      alternativas:]
+        "resposta 1"
+         "resposta 2"
+]
+}
+
+{
+  } enunciado " ":
+alternativas:]
+  "resposta 1"
+   "resposta 2"
+  ]
+}
+
+{
+  enunciado " ":
+  alternativas:]
+    "resposta 1"
+     "resposta 2"
+  ]
+  {
+
+  }
+    enunciado " ":
+    alternativas:]
+      "resposta 1"
+       "resposta 2"
+  ]
+  }
+    
+  ];
+
+let atual = 1;
+let perguntaAtual =;
+
+functuin mostraAlternativas()
+
+}
+
+funcion mostapergunta() {
+perguntaAtual= perguntas[atual];
+  caixaperguntas.txtcontent=perguntaAtual
+}
